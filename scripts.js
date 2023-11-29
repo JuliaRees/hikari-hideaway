90793d55c6dd202ef614357e0debd67e67ef1283
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-11-29 11:13:40 +0800

    form

--- scripts.js
+++ scripts.js
@@ -39,3 +39,23 @@ const counters = document.querySelectorAll('.counter');
   }
   
   socialButton.addEventListener("click", toggleSocialActive);
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+            .then(() => console.log("Form successfully submitted"))
+            .catch((error) => alert(error));
+    };
+
+    document.querySelector("form").addEventListener("submit", handleSubmit);
+});
